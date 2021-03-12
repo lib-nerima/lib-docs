4c8bdfd8b983a0f686cef0887e50253c56ef8544
lib-nerima <79825245+lib-nerima@users.noreply.github.com>
2021-03-12 13:33:51 +0900

    Add files via upload

---
+++ src/material-icons/expand-more.svelte
@@ -0,0 +1,7 @@
+<script>
+  export let size = "1em";
+  export let color = "currentColor";
+  export let viewBox = "0 0 24 24";
+</script>
+
+<svg fill="{color}" width="{size}" height="{size}" viewBox="{viewBox}"><path d="M0 0h24v24H0z" fill="none"/><path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z"/></svg>
